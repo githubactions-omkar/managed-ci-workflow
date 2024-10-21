adbe9b49adc1cfb91b725da88d206e12455297fa
Omkar Chavan <40963275+Omkarprakashchavan@users.noreply.github.com>
2024-10-21 16:03:11 +0530

    Update managed-ci-workflow.py

--- modules/managed-ci-workflow.py
+++ modules/managed-ci-workflow.py
@@ -98,56 +98,57 @@ repositories = []
 #     'Content-Type': 'application/json'
 # }
 
-logger: Union[logging.Logger, None] = None
-gh_obj = None
-topdir = os.path.dirname(os.path.abspath(sys.argv[0]))
-logdir = f'{topdir}/logdir'
-file_name_pattern='managed-ci'
-
-versioned_ci_repo = f'{os.path.dirname(__file__)}/../tarun-repo-config'
-arr = os.listdir()
-print(arr)
-print(f'versioned CI Repo {versioned_ci_repo}')
-repo_path = versioned_ci_repo
-# file_path = 'tarun-repo-config/configs/workflow-deployment.yaml'
-file_path = 'workflow-deployment.yaml'
-print(f' file path ------ {file_path}')
-mod_path = Path(__file__).parent
-relative_config_path = '../tarun-repo-config/'
-src_path_1 = (mod_path / relative_config_path).resolve()
-# deployment_workflow_path = str((mod_path / relative_config_path / 'configs' / 'workflow-deployment.yaml').resolve())
-deployment_workflow_path = 'configs/workflow-deployment.yaml'
-print(f'deployment_workflow_path ============= {deployment_workflow_path}')
-print(f'{mod_path},-------------------- {src_path_1}')
-repo_path = src_path_1
-print(f'printing version ci repo name {versioned_ci_repo} {repo_path}')
-repo = git.Repo(repo_path)
-try:
-  main_branch = repo.heads.main
-  print(f'main branch name {main_branch}')
-except AttributeError:
-  raise ValueError("The repository does not have a branch named 'main'.")
-
-latest_commit_sha = main_branch.commit.hexsha
-second_top_commit = get_second_top_commit(repo_path)
-print(f"Latest commit SHA of 'main': {latest_commit_sha}")
-try:
-  file_commit_sha = get_file_content_from_commit(repo, latest_commit_sha, deployment_workflow_path)
-  # print(f"Commit SHA of '{file_path}' in the latest commit: {file_commit_sha}")
-except ValueError:
-  print(f"File '{file_path}' does not exist in the latest commit {latest_commit_sha}")
-
-try:
-  content_old = get_file_content_from_commit(repo, second_top_commit, deployment_workflow_path)
-  content_new = get_file_content_from_commit(repo, latest_commit_sha, deployment_workflow_path)
-except ValueError as e:
-  print(e)
-
-dict_old = load_yaml(content_old)
-dict_new = load_yaml(content_new)
-# Extract repositories data from dict1 and dict2
-repositories1 = dict_old['modules'][0]['repositories']
-repositories2 = dict_new['modules'][0]['repositories']
-
-changed_repositories = compare_repositories(repositories1, repositories2)
-print(f'Changed repositories: {changed_repositories}')
+def main(module_name='', module_description='', repositories=[], default_managed_refspec=None):
+    logger: Union[logging.Logger, None] = None
+    gh_obj = None
+    topdir = os.path.dirname(os.path.abspath(sys.argv[0]))
+    logdir = f'{topdir}/logdir'
+    file_name_pattern='managed-ci'
+    
+    versioned_ci_repo = f'{os.path.dirname(__file__)}/../tarun-repo-config'
+    arr = os.listdir()
+    print(arr)
+    print(f'versioned CI Repo {versioned_ci_repo}')
+    repo_path = versioned_ci_repo
+    # file_path = 'tarun-repo-config/configs/workflow-deployment.yaml'
+    file_path = 'workflow-deployment.yaml'
+    print(f' file path ------ {file_path}')
+    mod_path = Path(__file__).parent
+    relative_config_path = '../tarun-repo-config/'
+    src_path_1 = (mod_path / relative_config_path).resolve()
+    # deployment_workflow_path = str((mod_path / relative_config_path / 'configs' / 'workflow-deployment.yaml').resolve())
+    deployment_workflow_path = 'configs/workflow-deployment.yaml'
+    print(f'deployment_workflow_path ============= {deployment_workflow_path}')
+    print(f'{mod_path},-------------------- {src_path_1}')
+    repo_path = src_path_1
+    print(f'printing version ci repo name {versioned_ci_repo} {repo_path}')
+    repo = git.Repo(repo_path)
+    try:
+      main_branch = repo.heads.main
+      print(f'main branch name {main_branch}')
+    except AttributeError:
+      raise ValueError("The repository does not have a branch named 'main'.")
+    
+    latest_commit_sha = main_branch.commit.hexsha
+    second_top_commit = get_second_top_commit(repo_path)
+    print(f"Latest commit SHA of 'main': {latest_commit_sha}")
+    try:
+      file_commit_sha = get_file_content_from_commit(repo, latest_commit_sha, deployment_workflow_path)
+      # print(f"Commit SHA of '{file_path}' in the latest commit: {file_commit_sha}")
+    except ValueError:
+      print(f"File '{file_path}' does not exist in the latest commit {latest_commit_sha}")
+    
+    try:
+      content_old = get_file_content_from_commit(repo, second_top_commit, deployment_workflow_path)
+      content_new = get_file_content_from_commit(repo, latest_commit_sha, deployment_workflow_path)
+    except ValueError as e:
+      print(e)
+    
+    dict_old = load_yaml(content_old)
+    dict_new = load_yaml(content_new)
+    # Extract repositories data from dict1 and dict2
+    repositories1 = dict_old['modules'][0]['repositories']
+    repositories2 = dict_new['modules'][0]['repositories']
+    
+    changed_repositories = compare_repositories(repositories1, repositories2)
+    print(f'Changed repositories: {changed_repositories}')
