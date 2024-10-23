71505941bc4a2201d8db50261b032e981437242c
Omkar Chavan <40963275+Omkarprakashchavan@users.noreply.github.com>
2024-10-23 20:48:16 +0530

    Update managed-ci-workflow.py

--- modules/managed-ci-workflow.py
+++ modules/managed-ci-workflow.py
@@ -3,138 +3,50 @@ import hashlib
 import logging
 import os
 import sys
-import git
-import yaml
-import subprocess
-import requests
-import json
-import utils.myutils as mu
-from os import listdir
-from os.path import isfile, join
 from typing import Dict, List, Union
 from datetime import datetime
+from pathlib import Path
+
+import yaml
 from ruamel.yaml import YAML
-from pathlib import Path
-from pathlib import Path
+
+sys.path.append(f'{os.path.dirname(__file__)}/..')
+import subprocess
+import utils.myutils as mu
 from utils.myutils import file_exists, mkdir_p
 from utils.github_apis import GitHubAPIs
-sys.path.append(f'{os.path.dirname(__file__)}/..')
+from os import listdir
+from os.path import isfile, join
+
+import requests
+import json
 
 api_url = 'https://api.github.com/graphql'
 github_token = os.environ['GITHUB_APP_TOKEN']
+# organisation = 'glcp'
 organisation = 'Omkarprakashchavan'
 repositories = []
-# headers = {
-#     'Authorization': f'Bearer {github_token}',
-#     'Content-Type': 'application/json'
-# }
+headers = {
+    'Authorization': f'Bearer {github_token}',
+    'Content-Type': 'application/json'
+}
+
+logger: Union[logging.Logger, None] = None
+gh_obj = None
+topdir = os.path.dirname(os.path.abspath(sys.argv[0]))
+logdir = f'{topdir}/logdir'
+file_name_pattern='managed-ci'
 
 def main(module_name='', module_description='', repositories=[], default_managed_refspec=None):
-    logger: Union[logging.Logger, None] = None
-    cwd = Path.cwd()
-    script_path = Path(__file__).parent
-    mci_path = '../../tarun-repo-config'
-    
-    mci_abs_path = (script_path / mci_path).resolve()
-    # print(f' mci_abs_path in managed-ci-workflow repo {mci_abs_path}')
-    
-    # versioned_ci_repo = f'{os.path.dirname(__file__)}/../../tarun-repo-config'
-
-    # print(f'versioned CI Repo {versioned_ci_repo}')
-    # repo_path = versioned_ci_repo
-    # file_path = 'tarun-repo-config/configs/workflow-deployment.yaml'
-    # file_path = 'workflow-deployment.yaml'
-    # print(f' file path ------ {file_path}')
-    script_path = Path(__file__).parent
-    print(f'printing script_path {script_path}')
-    relative_config_path = '../../tarun-repo-config/'
-    mci_abs_path = (script_path / relative_config_path).resolve()
-    # deployment_workflow_path = str((script_path / relative_config_path / 'configs' / 'workflow-deployment.yaml').resolve())
-    deployment_workflow_path = 'configs/workflow-deployment.yaml'
-    print(f'deployment_workflow_path ============= {deployment_workflow_path}')
-    print(f'{script_path},-------------------- {mci_abs_path}')
-    repo_path = mci_abs_path
-    
-    if os.environ['RUN_EVENT'] == 'push':
-        repo = git.Repo(repo_path)
-        try:
-          main_branch = repo.heads.main
-        except AttributeError:
-          raise ValueError("The repository does not have a branch named 'main'.")
-        
-        latest_commit_sha = main_branch.commit.hexsha
-        second_top_commit = get_second_top_commit(repo_path)
-        if not second_top_commit:
-            sys.exit("Unable to get second top commit for the managed-ci-workflow-config repository..")
-        print(f"Latest commit SHA of 'main': {latest_commit_sha}")
-        try:
-          file_commit_sha = get_file_content_from_commit(repo, latest_commit_sha, deployment_workflow_path)
-          # print(f"Commit SHA of '{deployment_workflow_path}' in the latest commit: {file_commit_sha}")
-        except ValueError:
-          print(f"File '{deployment_workflow_path}' does not exist in the latest commit {latest_commit_sha}")
-        if file_commit_sha:
-            try:
-                content_old = get_file_content_from_commit(repo, second_top_commit, deployment_workflow_path)
-                content_new = get_file_content_from_commit(repo, latest_commit_sha, deployment_workflow_path)
-            except ValueError as e:
-                print(e)
-        
-            dict_old = load_yaml(content_old)
-            dict_new = load_yaml(content_new)
-            # Extract repositories data from dict1 and dict2
-            repositories1 = dict_old['modules'][0]['repositories']
-            repositories2 = dict_new['modules'][0]['repositories']
-            
-            changed_repositories = compare_repositories(repositories1, repositories2)
-            print(f'Changed repositories: {changed_repositories}')
-            process_all_repo(module_name='', module_description='', repositories=changed_repositories.get('repositories'), default_managed_refspec=None)
-        else:
-            print(f"File '{deployment_workflow_path}' does not exist in the latest commit {latest_commit_sha}")
-    else:
-        logger.info("RUN EVENT is not a push event, hence running the script normally")
-        process_all_repo(module_name='', module_description='', repositories=repositories, default_managed_refspec=None)
-    
-def get_second_top_commit(repo_path):
-    repo = git.Repo(repo_path)
-    try:
-        branch = repo.heads.main
-    except AttributeError:
-        logger.error("The repository does not have a branch named 'main'.")
-        return False
-
-    commits = list(repo.iter_commits(branch.name, max_count=2))
-    print(commits)
-    if len(commits) < 2:
-        logger.error("There are less than two commits in the 'main' branch.")
-        return False
-
-    second_top_commit = commits[1]
-    commit = repo.commit(commits[0])
-    parent_commit = commit.parents[0] if commit.parents else None
-    if parent_commit:
-        diff = commit.diff(parent_commit)
-        changed_files = []
-        for change in diff:
-            file_info = {
-                'file': change.a_path,  # the file name
-                'change_type': change.change_type  # 'A', 'M', or 'D'
-            }
-            changed_files.append(file_info)
-        print(changed_files)
-    return second_top_commit
-
-def process_all_repo(module_name='', module_description='', repositories=[], default_managed_refspec=None):
-    """This Function processes all the repos passed to the function"""
     if not 'ORG_NAME' in os.environ:
-        org_name='glcp'
+        # org_name='glcp'
+        org_name='Omkarprakashchavan'
     else:
         org_name=os.environ['ORG_NAME']
     global managed_ci_workflow_repo
     managed_ci_workflow_repo='managed-ci-workflow'
 
     app_token = os.environ.get("GITHUB_APP_TOKEN", '')
-    topdir = os.path.dirname(os.path.abspath(sys.argv[0]))
-    logdir = f'{topdir}/logdir'
 
     mu.mkdir_p(logdir)
     global logger
@@ -147,7 +59,7 @@ def process_all_repo(module_name='', module_description='', repositories=[], def
     logger.debug(f'Final list of Repos in the glcp org')
 
     # sq_data: Dict[str, List[Dict[str,str]]] = \
-    #    # sonarqube_config(org_name=org_name)
+    #    sonarqube_config(org_name=org_name)
     # num_sq_projects = len(sq_data['Projects'])
 
     new_deploys={}
@@ -155,9 +67,7 @@ def process_all_repo(module_name='', module_description='', repositories=[], def
     for repo in repositories:
         r = repo.get('name')
         if r not in org_repos:
-            # raise Exception(f"Repository {r} not found in {org_name} organization")
-            print(f'{r} repo not found in {org_name}')
-            continue
+            raise Exception(f"Repository {r} not found in {org_name} organization")
         refspec = repo.get('refspec', default_managed_refspec)
         optional_workflows_requested = repo.get('optional_workflows', [])
         full_build_system = repo.get('build_system', [])
@@ -176,7 +86,10 @@ def process_all_repo(module_name='', module_description='', repositories=[], def
         if not clone_status:
             logger.error(f'Failed to clone {r} repositoroy for tag {refspec}. Hence skipping it...')
             continue
+        
         versioned_ci_repo = f'{os.path.dirname(__file__)}/../{r}/{managed_ci_workflow_repo}'
+        print(f'printing versioned_ci_repo {versioned_ci_repo}......')
+
         template_workflow_path =f'{versioned_ci_repo}/templates'
         primary_workflow_path =f'{versioned_ci_repo}/workflows'
         workflow_manifest_file =f'{versioned_ci_repo}/workflow-manifest.yaml'
@@ -274,7 +187,7 @@ def process_all_repo(module_name='', module_description='', repositories=[], def
         new_deploys[r]['refspec'] = refspec
         new_deploys[r]['workflows'] = [{'name': os.path.basename(wf), 'updated': timestamp} for wf in workflow_sources]
 
-        # sonarqube_config(sq_data, r, gh_obj.get_default_branch(r))
+        sonarqube_config(sq_data, r, gh_obj.get_default_branch(r))
 
     # if len(sq_data['Projects']) > num_sq_projects:
     #     sonarqube_config(sq_data, save=True)
@@ -282,8 +195,8 @@ def process_all_repo(module_name='', module_description='', repositories=[], def
     #     logger.debug('nothing to push... all repos are present in the SonarQube config file')
         
     repository_statuscheck_secrets(repositories)
-    update_log_file(new_deploys=new_deploys, old_deploys=old_deploys)
-       
+    # update_log_file(new_deploys=new_deploys, old_deploys=old_deploys)
+    
 def repository_statuscheck_secrets(repositories):
     '''This functions adds status checks and secrets to required repositories'''
     repository_list = [reps['name'] for reps in repositories]
@@ -752,11 +665,28 @@ def check_if_branch_protected(repository, repository_id, default_branch, refspec
                     protected_status_check_context = rule["requiredStatusCheckContexts"]
                     updated_status_check_context = evaluate_context_for_bpr(refspec, repository, protected_status_check_context)
                     branch_protection_rule(repository, default_branch, updated_status_check_context)
+        
+        # Check if branch protection rule exists for 'hotfix'
+        hotfix_bpr_exists = any(branch.lower().startswith('hotfix') for branch in protected_branches)
+        if hotfix_bpr_exists:
+            for item in data:
+                if item['pattern'].lower().startswith('hotfix'):
+                    protected_status_check_context = item['requiredStatusCheckContexts']
+                    protected_branch = item['pattern']
+                    print(f"{item['pattern']} exists in branch protection rule")
+        else:
+            protected_status_check_context = []
+            protected_branch = 'hotfix*'
+            print(f"hotfix does not exists in branch protection rule")
+        updated_status_check_context = evaluate_context_for_bpr(refspec, repository, protected_status_check_context)
+        branch_protection_rule(repository, protected_branch, updated_status_check_context)
+
     except requests.exceptions.RequestException as e:
         logger.debug(f"Failed to query GitHub GraphQL API. Status code: {response.text} {str(e)}")
         protected_status_check_context = []
         updated_status_check_context = evaluate_context_for_bpr(refspec, repository, protected_status_check_context)
         branch_protection_rule(repository, default_branch, updated_status_check_context)
+        branch_protection_rule(repository, 'hotfix*', updated_status_check_context)
 
 def remove_none_values(d):
     return {
@@ -904,38 +834,3 @@ def evaluate_context_for_bpr(refspec, repository, protected_status_check_context
     join_status_context = list(set(protected_status_check_list + required_status_check_contexts + tag_status_context + language_context))
     return join_status_context
     
-def load_yaml(file_content):
-    """Read workflow-deployment.yaml content into a dictionary."""
-    return yaml.safe_load(file_content)
-
-def get_file_content_from_commit(repo, commit_sha, file_path):
-    """Get the content of a file from a specific commit SHA."""
-    commit = repo.commit(commit_sha)
-    try:
-        file_blob = commit.tree[file_path]
-        file_content = file_blob.data_stream.read().decode('utf-8')
-        return file_content
-    except KeyError:
-        raise ValueError(f"File '{file_path}' does not exist in commit {commit_sha}")
-
-def compare_repositories(repo_list1, repo_list2):
-    """Compare two lists of repositories and return the differences."""
-    changes = {'repositories': []}
-    # Convert list of dictionaries to a list of unique identifiers (name and refspec)
-    def get_repo_identifiers(repo_list):
-        return {(repo.get('name'), repo.get('refspec')): repo for repo in repo_list}
-    # Convert both lists to identifiers
-    repo_dict1 = get_repo_identifiers(repo_list1)
-    repo_dict2 = get_repo_identifiers(repo_list2)
-    # Find repositories in repo_dict2 that are not in repo_dict1
-    for key, repo in repo_dict2.items():
-        if key not in repo_dict1:
-            changes['repositories'].append(repo)
-    # Find repositories that exist in both dicts but have different contents
-    for key in repo_dict1.keys():
-        if key in repo_dict2 and repo_dict1[key] != repo_dict2[key]:
-            changes['repositories'].append(repo_dict2[key])
-    # If no changes, return an empty dictionary
-    if not changes['repositories']:
-        return {}
-    return changes
