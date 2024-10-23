e538d0fc7d58398618f19078c3c5853cdfa81ce4
Omkar Chavan <40963275+Omkarprakashchavan@users.noreply.github.com>
2024-10-23 21:19:26 +0530

    Update managed-ci-workflow.py

--- modules/managed-ci-workflow.py
+++ modules/managed-ci-workflow.py
@@ -73,7 +73,10 @@ def main(module_name='', module_description='', repositories=[], default_managed
         optional_workflows_requested = repo.get('optional_workflows', [])
         full_build_system = repo.get('build_system', [])
         print(r, refspec, optional_workflows_requested, full_build_system)
-        build_system = full_build_system[0]
+        if len(build_system) > 1:
+            build_system = full_build_system[0]
+        else:
+            build_system = []
 
         if gh_obj.check_is_repo_archived(r):
             logger.info(f'Repo "{r}" is Archived ...Skipping')
